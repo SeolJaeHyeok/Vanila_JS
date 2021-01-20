86924dd1031c269451a845310d2cfd7e6c201703
seoljaehyeok <milkboy2564@naver.com>
2021-01-20 17:46:58 +0900

    Challenge #6

---
+++ Challenge#6/index.js
@@ -0,0 +1,38 @@
+const subtitle = document.querySelector(".subtitle"),
+  rangevalue = document.querySelector(".js-range"),
+  uservalue = document.querySelector(".js-value"),
+  messagebox = document.querySelector(".message"),
+  playBtn = document.querySelector(".playBtn"),
+  resultMessage = document.querySelector(".result");
+
+function userNumber() {
+  const range = rangevalue.value;
+  subtitle.innerText = `Generate a number between 0 and ${range}`;
+  return range;
+}
+
+function machineNumber() {
+  const rand = Date.now() % userNumber();
+  return rand;
+}
+
+function play(event) {
+  event.preventDefault();
+  const machineNum = machineNumber();
+  messagebox.innerText = `You chose : ${uservalue.value}, the machine chose : ${machineNum}.`;
+  if (parseInt(uservalue.value) === machineNum) {
+    resultMessage.innerText = `You won!`;
+  } else {
+    resultMessage.innerText = `You lost!`;
+  }
+}
+
+function init() {
+  userNumber();
+  machineNumber();
+}
+
+rangevalue.addEventListener("input", userNumber);
+playBtn.addEventListener("click", play);
+
+init();
