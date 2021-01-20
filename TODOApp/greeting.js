a81d9a28e4a4908ddd9f094d79e6a3c3b3f610b2
seoljaehyeok <milkboy2564@naver.com>
2021-01-20 16:20:51 +0900

    수정

--- TODOApp/greeting.js
+++ TODOApp/greeting.js
@@ -1,18 +1,12 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greeting");
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
-}
-
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello, ${text}`;
 }
 
 function handleSubmit(event) {
@@ -25,6 +19,12 @@ function handleSubmit(event) {
 function askForName() {
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
